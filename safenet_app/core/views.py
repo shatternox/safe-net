fc2c4375da9d785a4c09b3ac574a4c7f2c8e0437
Ardian Danny <65061019+shatternox@users.noreply.github.com>
2021-04-15 01:07:30 +0700

    test
    test

--- safenet_app/core/views.py
+++ safenet_app/core/views.py
@@ -8,4 +8,15 @@ core = Blueprint('core', __name__)
 @core.route("/index")
 def index():
 
-    return render_template("index.html")
+    return render_template("index.html", title="Home")
+
+
+@core.route("/about")
+def about():
+
+    return render_template("about.html", title="About")
+
+@core.route("/contact")
+def contact():
+
+    return render_template("contact.html", title="Contact")
