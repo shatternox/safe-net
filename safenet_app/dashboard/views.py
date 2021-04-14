4f616934cdfccb9f87813494eab6233fb36757a0
Ardian Danny <65061019+shatternox@users.noreply.github.com>
2021-04-14 22:14:14 +0700

    .
    .

--- safenet_app/dashboard/views.py
+++ safenet_app/dashboard/views.py
@@ -1,12 +1,10 @@
 from flask import render_template, request, Blueprint, current_app, session
 from safenet_app import app
 
-dashboard = Blueprint('dashboard', __name__)
+administration = Blueprint('administration', __name__)
 
-@dashboard.route("/dashboard", methods=['POST', 'GET'])
+@administration.route("/dashboard", methods=['POST', 'GET'])
 def dashboard():
-
-
 
     return render_template("dashboard.html", form=form)
 
